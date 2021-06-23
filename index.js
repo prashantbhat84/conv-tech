a00a15203e16640322429c5bf809e911490beaa0
Prashant Bhat <prashantbhat84@gmail.com>
2021-06-23 18:54:21 +0530

    added home route

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 const port = process.env.PORT || 6000;
-app.use((req, res, next) => {
-    if (req.method !== "POST") {
-        return res.status(400).json({ status: "false", result: "Only Post request allowed" })
-    }
-    next()
+// app.use((req, res, next) => {
+//     if (req.method !== "POST") {
+//         return res.status(400).json({ status: "false", result: "Only Post request allowed" })
+//     }
+//     next()
 
-});
+// });
+app.get("/", (req, res, next) => {
+    res.status(200).send("server is up")
+})
 app.use("/", require("./routes/routes"))
 
 
